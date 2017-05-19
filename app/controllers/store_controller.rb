0176ef97c1ae3c5bd491f710b88eb742fc2961df
Samseppiol <j_hage@live.com>
2017-05-19 12:11:36 +1000

    Finished the playtime activities, implemented a session counter that counts and displays how many times the user has visited the store index page. Then only displaying it if a user has visited more than five times

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @count = count
+  end
+
+  def count
+    session[:counter] ||= 0
+    session[:counter] += 1
   end
 end
