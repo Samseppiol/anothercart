ac15b07e856a54cceb4286682f4b4ecb4d2bef02
Samseppiol <j_hage@live.com>
2017-05-23 15:59:34 +1000

    trying to implement more ajax

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -68,6 +68,23 @@ end
     end
   end
 
+  def decrement
+    @cart = current_cart
+    @line_item = @cart.decrement_line_item_quantity(params[:id])
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to store_index_path, notice: 'Successfully updated'}
+        format.js {@current_item = @line_item}
+        format.json { head :ok}
+      else
+        format.html { render action: 'edit'}
+        format.js {@current_item = @line_item}
+        format.json { render json: @line_item.errors, status: :unprocessable_entity}
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_line_item
