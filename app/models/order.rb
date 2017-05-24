dc271780a846f6d424ead3e7aef99b368a492ae4
Samseppiol <j_hage@live.com>
2017-05-24 19:36:51 +1000

    Added model validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
 
 PAYMENT_TYPES = [ "Check", "Credit Card", "Purchase Order"]
+
+validates :name, :address, :email, presence: true 
+validates :pay_type, inclusion: PAYMENT_TYPES
 end
