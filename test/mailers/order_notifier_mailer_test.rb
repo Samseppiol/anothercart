364e57d1e2b378c694d2758074e2043c7a88aada
Samseppiol <j_hage@live.com>
2017-05-28 19:47:51 +1000

    Altered the two genereated tests for mailer. Failed a few times, once again rails telling me what is expected for the message, simply replacing the mistakes and the tests pass

--- test/mailers/order_notifier_mailer_test.rb
+++ test/mailers/order_notifier_mailer_test.rb
@@ -2,19 +2,17 @@ require 'test_helper'
 
 class OrderNotifierMailerTest < ActionMailer::TestCase
   test "recieved" do
-    mail = OrderNotifierMailer.recieved
-    assert_equal "Recieved", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    mail = OrderNotifierMailer.recieved(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["cr7thebest@live.com"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderNotifierMailer.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
+   mail = OrderNotifierMailer.recieved(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["cr7thebest@live.com"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
   end
 
 end
