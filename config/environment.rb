4f45c65386301205c7857b5060a666464ea1591a
Samseppiol <j_hage@live.com>
2017-05-28 19:23:33 +1000

    Used dotenv to secure all my details needed for the mailer, set up the correct config settings. The mailer works, pretty much instant email.

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,15 @@ require_relative 'application'
 Rails.application.initialize!
 
 
-# Depot::Application.configure do 
-#     config.action_mailer.delivery_method = :smtp
-#   config.action_mailer.smtp_settings = { }
-# end
+Anothercart::Application.configure do
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: ENV.fetch('gmail_address'),
+      port: ENV.fetch('port'),
+      domain: ENV.fetch('domain'),
+      authentication: ENV.fetch('authentication'),
+      user_name: ENV.fetch('user_name'),
+      password: ENV.fetch('password'),
+      enable_starttls_auto: true
+    }
+end
